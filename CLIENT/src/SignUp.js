dfa8a2c0f31d5683852939d3f38f6d575c97c927
Tumelo Mahlangu <tumelom@bbd.co.za>
2022-04-21 16:46:57 +0200

    updated the client code

--- CLIENT/src/SignUp.js
+++ CLIENT/src/SignUp.js
@@ -1,26 +1,42 @@
-const username = document.getElementById("name");
-const lastName = document.getElementById("lastname");
-const contact = document.getElementById("contact");
-const email = document.getElementById("email");
-const password = document.getElementById("password");
+const username = document.getElementById("name").value;
+const lastName = document.getElementById("lastname").value;
+const contact = document.getElementById("contact").value;
+const email = document.getElementById("email").value;
+const address = document.getElementById("address").value;
+const password = document.getElementById("password").value;
 
-console.log(username)
 
+const submitButton = document.getElementById("submit-button")
 
+submitButton.addEventListener("click",()=>{
+    insertCustomerAPI()
+    console.log("Event called!!")
+})
+
+console.log({
+    "name" : username,
+    "lastname" : lastName,
+    "contact" :contact,
+    "email" : email,
+    "address" : address,
+    "password" : password
+})
 const insertCustomerAPI = () => {
-    fetch('/', {
+    fetch('http://localhost:3000/addCustomers', {
         method: 'POST',
-        headers: {
-            Authorization: 'Bearer abcdxyz',
-            'Content-Type': 'application/json',
+        headers: {"Accept": "application/json","Content-type": "application/json"},
+        body: {
+            "name" : username,
+            "lastname" : lastName,
+            "contact" :contact,
+            "email" : email,
+            "address": address,
+            "password" : password
         },
-        body: JSON.stringify({
-            username,
-            password,
-        }),
     })
     .then((res) => {
         return res.json();
     })
     .then((data) => console.log(data));
 }
+
--- CLIENT/src/script.js
+++ CLIENT/src/script.js
@@ -1,19 +1,26 @@
 
-
-console.log(username)
 const getAllProducts = () => {
     fetch('http://localhost:3000/products')
     .then(response => response.json())
     .then(data => CreateCard(data))
 }
+
+const getAllProducts1 = () => {
+    fetch('http://localhost:3000/products')
+    .then(response => response.json())
+    .then(data => CreateCard1(data))
+}
 getAllProducts()
+getAllProducts1()
 
-
+// getAllProducts().map((item) => {
+//     console.log(item)
+// })
 
 const CreateCard = (data) =>{ 
-    console.log(data.length)
+    console.log(data)
     data.map(item => {
-       return `
+        `
             <article>
                 <img src="" alt="">
                 <p>${item.C}</p>
@@ -23,54 +30,123 @@ const CreateCard = (data) =>{
         `
     })
 
-    console.log(CreateCard())
-
-
     /*const para=document.createElement("p");
     const paraText=document.createTextNode(data[0].P[0])
     para.appendChild(paraText);
     document.getElementById("product").appendChild(para)
-    document.body.appendChild(para)*/
+    //document.body.appendChild(para)*/
 
-    for(let i=0; i<data.length;i++){
-        const productCard = document.createElement("article")
-        const para = document.createElement("p")
+    const imagesArray = ["../assets/ItemImages/Beef.png","../assets/ItemImages/onions.png","../assets/ItemImages/Royal.png","../assets/ItemImages/Bananas.png","../assets/ItemImages/Rajah.png","../assets/ItemImages/milk.png","../assets/ItemImages/Doritos.png","../assets/ItemImages/Bread.png"];
+    var count=1;
+    for(var i=6; i<=9;i++){
+       
+        
+//----------Category----------------
 
-        const paraText = document.createTextNode(data[i].C)
-        para.appendChild(paraText);
-        document.getElementById("product-section").appendChild(para)
-        console.log(para);
+    const paraArt=document.createElement("article")
+    const para=document.createElement("p")
+    const paraText=document.createTextNode(data[i].C)
+    para.appendChild(paraText);
+    paraArt.appendChild(para)
+   
+    //console.log(paraArt);
+/////////////////////////////////////////////////
 
-    // console.log(data.length)
+//------------image------------------------
 
-        for(let x=0; x<2;x++){
+    var img = document.createElement('img');
+    img.src = imagesArray[i-6];
+    //img.appendChild(img.src);
+    document.getElementById("product-section-1").appendChild(img)
+    paraArt.appendChild(img)
+/////////////////////////////////////////////////
+
+    //console.log(img)
+
+    var count1=1;
+
+    for(var x=0; x<2;x++)
+    {
+
+        //---------------Product name and price----------------
+
+        if(count1==1)
+        {
+            const head3=document.createElement("h4");
+            const head3Text=document.createTextNode(data[i].P[x])
+            head3.appendChild(head3Text);
+           // paraArt1.appendChild(head3)
+            paraArt.appendChild(head3)
+        }
+        else
+        {
             const head3=document.createElement("h3");
             const head3Text=document.createTextNode(data[i].P[x])
             head3.appendChild(head3Text);
-            document.getElementById("product-section").appendChild(head3)
-            console.log(head3);
+           // paraArt1.appendChild(head3)
+            paraArt.appendChild(head3)
         }
-    }
-}
 
-const product = document.getElementById("product")
-product.innerHTML = ""
-
-
-console.log(CreateCard())
-
-const ShowData = (data) => {
-    const productName = document.getElementById("name")
-    const price = document.getElementById("price")
-    let arr = [];
-    
-    console.log(data)
-    
-    productName.innerHTML += data[0].P[0]
-    price.innerHTML += data[1].P[1]
-    console.log(product.Price)
-    const node = CreateCard(product);
-    return data
+        count1++;
        
-    
+    }
+    document.getElementById("product-section-1").appendChild(paraArt)
+    console.log(paraArt);
+
 }
+console.log(document.getElementById("product-section-1"));
+}
+
+//second artice
+
+const CreateCard1 = (data) =>{ 
+    console.log(data)
+
+    const imagesArray = ["../assets/ItemImages/Beef.png","../assets/ItemImages/onions.png","../assets/ItemImages/Royal.png","../assets/ItemImages/Bananas.png","../assets/ItemImages/Rajah.png","../assets/ItemImages/milk.png","../assets/ItemImages/Doritos.png","../assets/ItemImages/Bread.png"];
+    var count=1;
+    for(var i=10; i<data.length;i++){
+       
+        
+//----------Category----------------
+const paraArt1=document.createElement("article")
+    const para=document.createElement("p")
+    const paraText=document.createTextNode(data[i].C)
+    para.appendChild(paraText);
+    document.getElementById("product-section-2").appendChild(para)
+    paraArt1.appendChild(para)
+   
+
+//------------image------------------------
+
+    var img = document.createElement('img');
+    img.src = imagesArray[i-6];
+    //img.appendChild(img.src);
+    document.getElementById("product-section-2").appendChild(img)
+    paraArt1.appendChild(img)
+/////////////////////////////////////////////////
+
+   // console.log(img)
+
+    for(var x=0; x<2;x++)
+    {
+
+        //---------------Product name and price----------------
+        const head3=document.createElement("h3");
+        const head3Text=document.createTextNode(data[i].P[x])
+        head3.appendChild(head3Text);
+        document.getElementById("product-section-2").appendChild(head3)
+        console.log(head3);
+        paraArt1.appendChild(head3)
+    }
+    document.getElementById("product-section-2").appendChild(paraArt1)
+
+    console.log(paraArt1)
+}
+console.log(document.getElementById("product-section-2"))
+}
+
+// const product = document.getElementById("product")
+// product.innerHTML = ""
+
+
+//console.log(CreateCard())
